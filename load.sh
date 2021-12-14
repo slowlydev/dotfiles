40776f4f986eaa724cee6423c962de33c4606280
Slowlydev <slowly.developers@gmail.com>
2021-12-14 22:01:17 +0100

    update: made load script working

--- load.sh
+++ load.sh
@@ -2,67 +2,67 @@
 
 echo "creating folders ..."
 
-mkdir -p ~/.config/kitty/
+mkdir -p -v ~/.config/kitty/
 echo ": kitty"
 
-mkdir ~/.config/rofi/
+mkdir -p -v ~/.config/rofi/
 echo ": rofi"
 
-mkdir ~/.config/polybar
+mkdir -p -v ~/.config/polybar/
 echo ": polybar"
 
-mkdir ~/.config/dunst
+mkdir -p -v ~/.config/dunst/
 echo ": dunst"
 
-mkdir ~/.config/sxhkd
+mkdir -p -v ~/.config/sxhkd/
 echo ": sxhkd"
 
-mkdir ~/.backgrounds
+mkdir -p -v ~/.backgrounds/
 echo ": backgrounds"
 
-mkdir ~/.scripts
+mkdir -p -v ~/.scripts/
 echo ": scripts"
 
-mkdir ~/.config/bspwm/
+mkdir -p -v ~/.config/bspwm/
 echo ": bspwm"
 
-mkdir ~/.config/picom/
+mkdir -p -v ~/.config/picom/
 echo ": picom"
 
 echo "loading dotfiles..."
 
-cp ./kitty/ ~/.config/kitty/
+cp ./kitty/* ~/.config/kitty/
 echo ": kitty"
 
-cp ./rofi/ ~/.config/rofi/
+cp ./rofi/* ~/.config/rofi/
 echo ": rofi"
 
-cp ./polybar/ ~/.config/polybar/
+cp ./polybar/* ~/.config/polybar/
 echo ": polybar"
 
-cp ./dunst/ ~/.config/dunst/
+cp ./dunst/* ~/.config/dunst/
 echo ": dunst"
 
-cp ./sxhkd/ ~/.config/sxhkd/
+cp ./sxhkd/* ~/.config/sxhkd/
 echo ": sxhkd"
 
-cp ./backgrounds/ ~/.backgrounds/
+cp ./backgrounds/* ~/.backgrounds/
 echo ": backgrounds"
 
-cp ./scripts/ ~/.scripts/
+cp ./scripts/* ~/.scripts/
 echo ": scripts"
 
-cp ./bspwm/ ~/.config/bspwm
+cp ./bspwm/* ~/.config/bspwm/
 echo ": bspwm"
 
-cp ./picom/ ~/.config/picom
-echo ": picom"
+cp ./picom/* ~/.config/picom/
+echo ": picom - picom.conf"
 
-cp ./bashrc/ ~/.bashrc
-echo ": bashrc"
+cp ./bashrc/bashrc ~/.bashrc
+echo ": bash - .bashrc"
 
-cp ./x/Xresources/ ~/.Xresources
-echo ": .Xrsources"
+cp ./x/Xresources ~/.Xresources
+echo ": x - .Xresources"
 
-cp ./oh-my-posh/ ~/.slowlydev.omp.json
-echo ": oh-my-posh"
+cp ./oh-my-posh/.slowlydev.omp.json ~/.slowlydev.omp.json
+echo ": oh-my-posh - .slowlydev.omp.json"
