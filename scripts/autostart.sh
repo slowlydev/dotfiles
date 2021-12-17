fbb4cced71276f952b145f5ab751d094438a65e2
Slowlydev <slowly.developers@gmail.com>
2021-12-17 17:48:54 +0100

    update: adjusted configs to load my custom ansi layout

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,9 @@
 xsetroot -cursor_name left_ptr
-setxkbmap -layout ch
+
+setxkbmap -layout us
+
+xmodmap -e 'keycode 29 = Z'
+xmodmap -e 'keycode 52 = Y'
+
 xinput --set-prop 8 'libinput Accel Speed' -0.7
 xinput --set-prop 20 'libinput Accel Speed' -0.8
