785b5411533133d8f21a6ba36a5f41fb1b6e6300
Slowlydev <slowly.developers@gmail.com>
2022-02-26 12:09:39 +0100

    save: before macos?

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,5 +5,6 @@ setxkbmap -layout us
 xmodmap -e 'keycode 29 = Z'
 xmodmap -e 'keycode 52 = Y'
 
-xinput --set-prop 8 'libinput Accel Speed' -0.7
-xinput --set-prop 20 'libinput Accel Speed' -0.8
+xinput --set-prop 11 'libinput Accel Speed' -0.8
+
+udiskie -2 &
