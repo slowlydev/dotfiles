ec1ad020192d394646327810acf8fe4fc23677b0
Slowlydev <slowly.developers@gmail.com>
2023-07-05 18:33:08 +0200

    feat: more saving dotfiles

--- load.sh
+++ load.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 
+cp -r ./gtk/gtk-2.0/ ~/.config/
 cp -r ./gtk/gtk-3.0/ ~/.config/
-cp -r /gtk/gtk-4.0/ ~/.config/
+cp -r ./gtk/gtk-4.0/ ~/.config/
+
+cp -r ./alacritty ~/.config/
+cp -r ./helix ~/.config/
 
 cp -r ./.icons ~/
 cp -r ./wallpapers ~/
--- save.sh
+++ save.sh
@@ -1,7 +1,11 @@
 #/bin/bash
 
+cp -r ~/.config/gtk-2.0/ ./gtk/
 cp -r ~/.config/gtk-3.0/ ./gtk/
 cp -r ~/.config/gtk-4.0/ ./gtk/
+
+cp -r ~/.config/alacritty .
+cp -r ~/.config/helix .
 
 cp -r ~/.icons/ .
 cp -r ~/wallpapers/ .
